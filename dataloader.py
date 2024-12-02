ce280e439ee3f23e93273156c0d7f2a19f246228
Xaelda <92032588+Xaelda@users.noreply.github.com>
2024-12-02 14:53:06 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,8 +4,7 @@ import pandas as pd
 import pyarrow
 import fastparquet
 
-# to get truncate_history, create_binary_labels_column, sampling_strategy_wu2019
-from _behaviors import *
+from ebrec.utils._behaviors import (create_binary_labels_column, truncate_history, sampling_strategy_wu2019)
 
 # TODO join behaviors with history
 
@@ -16,76 +15,48 @@ class Data:
         self.history = pl.read_parquet(path_to_history)
         self.article_embeddings = pd.read_parquet(path_to_embeddings)
         self.article_embeddings_dict = {row["article_id"]: row["contrastive_vector"] for row in self.article_embeddings.to_dict(orient='records')}
+        self.hparams = params
 
         # make dataframe for training
         COLUMNS_FROM_HISTORY = ["user_id", "article_id_fixed"]
         COLUMNS_FROM_BEHAVIORS = ["user_id", "impression_id", "impression_time", "article_ids_clicked", "article_ids_inview"]
-        COLUMNS = COLUMNS_FROM_HISTORY + COLUMNS_FROM_BEHAVIORS
 
-        HISTORY_SIZE = params.history_size #TODO make as global variable
-        NPRATIO = params.negative_sampling_ratio       #TODO make as gl...
-        SEED = params.seed        #TODO make as gl...
+        self._prepare_history(COLUMNS_FROM_HISTORY)
+        self._prepare_behaviors(COLUMNS_FROM_BEHAVIORS)
+        self._join_dataframes()
 
-        self.df_train = self.history.select(COLUMNS_FROM_HISTORY).pipe(
+    def _prepare_history(self, keep_columns):
+        self.history = (
+            self.history
+            .select(keep_columns)                               # Selecting the columns we want to keep
+            .rename({"article_id_fixed": "his_article_ids"})    # using prefix: his_ to indicate origin: history
+            .pipe(                                                         # Truncating the history
                     truncate_history,
-                    column="article_id_fixed",
-                    history_size=HISTORY_SIZE,
+                    column="his_article_ids",
+                    history_size=self.hparams.history_size,
                     padding_value=0,
                     enable_warning=False,
-                ).pipe(
-                    slice_join_dataframes,
-                    df2=self.behaviors.select(COLUMNS_FROM_BEHAVIORS),
-                    on="user_id",
-                    how="left",
-                ).pipe(
-                    sampling_strategy_wu2019,
-                    npratio=NPRATIO,
-                    shuffle=False,
-                    with_replacement=True,
-                    seed=SEED,
-                ).pipe(
-                    create_binary_labels_column,
-                    shuffle=True,
-                    seed=SEED,
                 )
-        '''
-        title_in_impression = self.behaviors[["user_id", "article_ids_inview", "article_ids_clicked"]]
-        title_in_impression_grouped_user_id = title_in_impression.set_index("user_id")
+            )
 
-        # Remove duplicates in index by making new list of article_ids_inview and article_ids_clicked
-        duplicates_bool = title_in_impression_grouped_user_id.index.duplicated(keep="first")
-        new_df = title_in_impression_grouped_user_id[~duplicates_bool]
-        df_duplicates = title_in_impression_grouped_user_id[duplicates_bool]
-        for index, row in df_duplicates.iterrows():
-            new_df.at[index, "article_ids_inview"] = list(
-                set(np.append(new_df.loc[index, "article_ids_inview"], row["article_ids_inview"])))
-            new_df.at[index, "article_ids_clicked"] = list(
-                set(np.append(new_df.loc[index, "article_ids_clicked"], row["article_ids_clicked"])))
+    def _prepare_behaviors(self, keep_columns):
+        self.behaviors = (
+            self.behaviors
+            .select(keep_columns) # selecting the columns we want to keep
+            .with_columns(
+                length=pl.col('article_ids_clicked').map_elements(lambda x: len(x)))  # adding a column with the length of the clicked articles
+            .filter(pl.col('length') == 1)  # we only want users with exactly one click in their impression
+            .pipe(sampling_strategy_wu2019, npratio=self.haparams.negative_sampling_ratio, shuffle=True, clicked_col="article_ids_clicked",
+                inview_col="article_ids_inview", with_replacement=False, seed=self.hparams.seed)   # down-sampling
+            .pipe(create_binary_labels_column, clicked_col="article_ids_clicked",      # creating the binary labels column
+                inview_col="article_ids_inview")
+            .drop("length")
+            )
 
-        new_df["article_ids_inview"].apply(tuple)
-        new_df["article_ids_clicked"].apply(tuple)
-        not_clicked = [
-            set(row["article_ids_inview"]).difference(set(row["article_ids_clicked"]))
-            for index, row in new_df.iterrows()
-        ]
-        new_df.loc[:, "article_ids_not_clicked"] = pd.Series(not_clicked, index=new_df.index)
-        self.user_click_information = new_df
-'''
-
-    def downsample(self,
-                   npratio  = 4,        # This is K
-                   seed     = 123):
-
-        " Downsample the article inview column to have npratio not clicked per one clicked "
-
-        self.behaviors = self.behaviors.with_columns("article_ids_inview", "article_ids_clicked").pipe(
-            sampling_strategy_wu2019,
-            npratio=npratio,
-            shuffle=False,
-            with_replacement=True,
-            seed=seed,
-        ).pipe(create_binary_labels_column, shuffle=True, seed=seed).with_columns(
-            pl.col("article_label_clicked").list.len().name.suffix("_len")
+    def _join_dataframes(self):
+        self.df_train = (
+            self.behaviors
+            .join(self.history, on="user_id", how="left") # joining the history and behaviors dataframes
         )
 
 
